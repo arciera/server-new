3751596ad2b3a7dbd2d4038b1fc36125c164ba84
Zefir Kirilov <contact@zefir.pro>
2024-06-06 15:35:28 +0300

    improve VarInt reading

--- src/Packet.ts
+++ src/Packet.ts
@@ -25,29 +25,27 @@ export class Packet {
      * A VarInt is read from the beginning of the packet and compared to the packet's current buffer length.
      */
     public isComplete(): boolean {
-        return this.buffer().byteLength === Packet.parseVarInt(this.buffer()).value;
+        return this.buffer().byteLength === Packet.readVarInt(this.buffer()).value;
     }
 
     /**
      * Parse VarInt from the beginning of a buffer
      * @param data
+     * @param offset
      */
-    public static parseVarInt(data: Buffer): Packet.ParseResult<number> {
-        let result = 0;
+    public static readVarInt(data: Buffer, offset: number = 0): Packet.ParseResult<number> {
+        let value = 0;
         let shift = 0;
-        let index = 0;
+        let byte = 0;
+        let index = offset;
 
-        while (true) {
-            const byte = data[index++]!;
-            result |= (byte & 0x7F) << shift;
+        do {
+            byte = data[index++]!;
+            value += (byte & 0x7F) << shift;
             shift += 7;
+        } while (byte & 0x80);
 
-            if ((byte & 0x80) === 0) {
-                break;
-            }
-        }
-
-        return new Packet.ParseResult(result, index);
+        return new Packet.ParseResult(value, index - offset);
     }
 
     /**
